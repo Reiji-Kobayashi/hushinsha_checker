8bb1381d764001768b207f0c175db9dcc00e07eb
Reiji Kobayashi <0zi.kobayashi@gmail.com>
2018-09-01 19:26:23 +0900

    add loop

--- sample.py
+++ sample.py
@@ -1,25 +1,29 @@
 from firebase import firebase
-from main import main_act
+from act import main_act
 import requests
 
-firebase = firebase.FirebaseApplication("https://iotiot-ae8e8.firebaseio.com/", None)
-result = firebase.get('/Nefry', None)
+
 
 #yxox0zzREiJiY6ArWwv56hxZbCjyERs9l3eABo4woyx
 line_notify_token = 'yxox0zzREiJiY6ArWwv56hxZbCjyERs9l3eABo4woyx'
 line_notify_api = 'https://notify-api.line.me/api/notify'
 message = '誰か来ました'
 
-#jsonをパースする
-response = result["SW"]
+firebase = firebase.FirebaseApplication("https://iotiot-ae8e8.firebaseio.com/", None)
 
-if response == "1":
-    #来訪者あり
-    main_act()
-    #print("来訪者あり")
-else:
-    #来訪者なし
-    print("来訪者なし")
+while(1):
+    result = firebase.get('/Nefry', None)
+
+    #jsonをパースする
+    response = result["SW"]
+    if response == "1":
+        #来訪者あり
+        main_act()
+        #print("来訪者あり")
+        break
+    else:
+        #来訪者なし
+        print("来訪者なし")
 
 
 payload = {'message': message}
